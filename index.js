02c12bede8e1fd7b237b47640f3a231c07f17c06
Vitor <vitorcabeca2005@hotmail.com>
2020-01-27 15:53:04 -0300

    Added middlewares to verify id && count requests

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 app.use(express.json())
 
 var projects = []
+var countRequest = 0
+
+app.use((req,res,next)=>{
+    countRequest++
+    console.log("Total de requisições: " + countRequest);
+    return next()
+})
+function checkUserExists(req,res,next) {
+    const {id} = req.params
+    const project = projects.find(proj => proj.id == id)
+    if(!projects){
+        return res.status(400).json({erros:"Project not exists"})
+    }
+    return next()
+}
 
 app.post("/projects",(req,res)=>{
     const {id,title} = req.body;
@@ -12,19 +27,19 @@ app.post("/projects",(req,res)=>{
 app.get("/projects",(req,res)=>{
     return res.status(200).json(projects)
 })
-app.put("/projects/:id",(req,res)=>{
+app.put("/projects/:id",checkUserExists,(req,res)=>{
     const {title} = req.body;
     const {id} = req.params;
     projects[id].title = title;
     return res.status(200).json(projects[id]);
 })
-app.delete("/projects/:id",(req,res)=>{
+app.delete("/projects/:id",checkUserExists,(req,res)=>{
     const {id} = req.params;
     projects.splice(id,1);
     return res.send()
 })
 
-app.post("/projects/:id/tasks",(req,res)=>{
+app.post("/projects/:id/tasks",checkUserExists,(req,res)=>{
     const {id} = req.params;
     const {title} = req.body;
     projects[id].tasks.push(title);
