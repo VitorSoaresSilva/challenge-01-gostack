978eccab69c2cae9f5ab16310cb9365623e318d6
Vitor <vitorcabeca2005@hotmail.com>
2020-01-27 16:52:48 -0300

    Fixed the way to get the project by ID

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req,res,next)=>{
 function checkUserExists(req,res,next) {
     const {id} = req.params
     const project = projects.find(proj => proj.id == id)
-    if(!projects){
+    if(!project){
         return res.status(400).json({erros:"Project not exists"})
     }
     return next()
@@ -21,29 +21,30 @@ function checkUserExists(req,res,next) {
 
 app.post("/projects",(req,res)=>{
     const {id,title} = req.body;
-    projects.push({"id":id,"title":title,"tasks":[]})
+    projects.push({id,title,"tasks":[]})
     return res.status(200).json(projects)
 })
 app.get("/projects",(req,res)=>{
     return res.status(200).json(projects)
 })
 app.put("/projects/:id",checkUserExists,(req,res)=>{
-    const {title} = req.body;
-    const {id} = req.params;
-    projects[id].title = title;
-    return res.status(200).json(projects[id]);
+    const {body:{title},params:{id}} = req
+    const project = projects.find(proj => proj.id == id)
+    project.title = title;
+    return res.status(200).json(project);
 })
 app.delete("/projects/:id",checkUserExists,(req,res)=>{
     const {id} = req.params;
-    projects.splice(id,1);
+    const index = projects.findIndex(proj => proj.id == id)
+    projects.splice(index,1);
     return res.send()
 })
 
 app.post("/projects/:id/tasks",checkUserExists,(req,res)=>{
-    const {id} = req.params;
-    const {title} = req.body;
-    projects[id].tasks.push(title);
-    return res.status(200).json(projects[id])
+    const {body:{title},params:{id}} = req
+    const project = projects.find(proj => proj.id == id)
+    project.tasks.push(title);
+    return res.status(200).json(project)
 })
 
 app.listen(3000, ()=>{
